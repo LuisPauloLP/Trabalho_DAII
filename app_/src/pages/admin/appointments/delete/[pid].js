3d69dfe9653acbe3feaf9f27d8fc37396273804e
mafe2205 <120610736+mafe2205@users.noreply.github.com>
2024-11-30 10:05:24 -0300

    adicionado delete agendamentos funcionando

--- app_/src/pages/admin/appointments/delete/[pid].js
+++ app_/src/pages/admin/appointments/delete/[pid].js
@@ -5,19 +5,21 @@ import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { useRouter } from 'next/router';
-import MenuAppointments from '../../../../components/MenuAppointments';
 
 export default function DeleteAppointment() {
   const API_URL = "http://localhost:3030/api/appointments/";
 
   const [appointment, setAppointment] = useState({
     appointment_id: "",
-    appointment_student_name: " ",
-    appointment_professional_name: " ",
+    appointment_student_name: "",
+    appointment_student_surmane: "",
+    appointment_professional_name: "",
+    appointment_professional_surname: "",
     appointment_professional_speciality: "",
-    appointment_date: " ",
-    appointment_comments: " ",
-    appointment_create_date: " "
+    appointment_date: "",
+    appointment_time: "",
+    appointment_comments: "",
+    appointment_create_date: ""
   });
 
   const router = useRouter();
@@ -26,12 +28,19 @@ export default function DeleteAppointment() {
   const [message, setMessage] = useState({ message: "", status: "" });
 
   useEffect(() => {
+    if (!pid) return;
+
     const getAppointment = async () => {
-      if (!pid) return;
       try {
         const response = await Axios.get(API_URL + pid);
+        const fetchedAppointment = response.data.foundedAppointment;
+
+        if (fetchedAppointment.appointment_date) {
+          fetchedAppointment.appointment_date = new Date(fetchedAppointment.appointment_date).toISOString().split("T")[0];
+        }
+
         setMessage({ message: response.data.message, status: "ok" });
-        setAppointment(response.data.foundedAppointment);
+        setAppointment(fetchedAppointment);
       } catch (error) {
         console.error('Erro ao buscar o agendamento:', error);
         setMessage({ message: "Erro ao buscar o agendamento!", status: "error" });
@@ -42,10 +51,12 @@ export default function DeleteAppointment() {
   }, [pid]);
 
   const handleDeleteAppointment = async () => {
+    if (!confirm("Tem certeza que deseja deletar este evento?")) return;
+
     try {
       const response = await Axios.delete(API_URL + pid);
       setMessage({ message: response.data.message, status: "ok" });
-      router.push("/admin/appointments");
+      router.push('/admin/appointments');
     } catch (error) {
       console.error("Erro ao deletar o agendamento:", error);
       setMessage({ message: "Erro ao deletar o agendamento!", status: "error" });
@@ -62,10 +73,15 @@ export default function DeleteAppointment() {
       <div>
       <NavAdmin />
       <MenuAppointments /> 
-        { 
-          message.status === "" ? "" : 
-          message.status === "ok" ? <div className='alert alert-success' role='alert'> { message.message } <Link className='alert-link' href='/admin/appointments'>Voltar</Link></div> : 
-          <div className='alert alert-danger' role='alert'> { message.message } <Link className='alert-link' href='/admin/appointments'>Voltar</Link></div>
+      {
+          message.status === "" ? "" :
+            message.status === "ok" ? 
+              <div className="alert alert-success" role="alert"> 
+                {message.message} <Link className="alert-link" href="/admin/appointments">Voltar</Link>
+              </div> : 
+              <div className="alert alert-danger" role="alert"> 
+                {message.message} <Link className="alert-link" href="/admin/appointments">Voltar</Link>
+              </div>
         }
       </div>
 
@@ -78,26 +94,38 @@ export default function DeleteAppointment() {
                 <label className="form-label" htmlFor="student_name">Nome do Estudante</label>
                 <input type="text" id="student_name" name="student_name" className="form-control" value={appointment.appointment_student_name} readOnly />
               </div>
-                <div className="form-group">
-                    <label className="form-label" htmlFor="appointment_professional_name">Nome do Profissional</label>
-                    <input type="text" id="appointment_professional_name" name="appointment_professional_name" className="form-control" value={appointment.appointment_professional_name} readOnly/>
+              <div className="form-group">
+                    <label className="form-label" htmlFor="appointment_student_surmane">Sobrenome do Estudante</label>
+                    <input type="text" id="appointment_student_surmane" name="appointment_student_surmane" className="form-control" value={appointment.appointment_student_surmane} readOnly />
+              </div>
+              <div className="form-group">
+                <label className="form-label" htmlFor="appointment_professional_name">Nome do Profissional</label>
+                <input type="text" id="appointment_professional_name" name="appointment_professional_name" className="form-control" value={appointment.appointment_professional_name} readOnly/>
+              </div>
+              <div className="form-group">
+                    <label className="form-label" htmlFor="appointment_professional_surname">Sobrenome do Profissional</label>
+                    <input type="text" id="appointment_professional_surname" name="appointment_professional_surname" className="form-control" value={appointment.appointment_professional_surname} readOnly />
+              </div>
+              <div className="form-group">
+                <label className="form-label" htmlFor="appointment_professional_speciality">Especialidade do Profissional</label>
+                <input type="text" id="appointment_professional_speciality" name="appointment_professional_speciality" className="form-control" value={appointment.appointment_professional_speciality} readOnly />
+              </div>
+              <div className="form-group">
+                <label className="form-label" htmlFor="appointment_date">Data do Agendamento</label>
+                <input type="date" id="appointment_date" name="appointment_date" className="form-control" value={appointment.appointment_date} readOnly />
+              </div>
+              <div className="form-group">
+                    <label className="form-label" htmlFor="appointment_time">Hora</label>
+                    <input type="time" id="appointment_time" name="appointment_time" className="form-control" value={appointment.appointment_time} readOnly />
                 </div>
-                <div className="form-group">
-                    <label className="form-label" htmlFor="appointment_professional_speciality">Especialidade do Profissional</label>
-                    <input type="text" id="appointment_professional_speciality" name="appointment_professional_speciality" className="form-control" value={appointment.appointment_professional_speciality} readOnly />
-                </div>
-                <div className="form-group">
-                    <label className="form-label" htmlFor="appointment_date">Data do Agendamento</label>
-                    <input type="date" id="appointment_date" name="appointment_date" className="form-control" value={appointment.appointment_date} readOnly />
-                </div>
-                <div className="form-group">
-                    <label className="form-label" htmlFor="appointment_comments">Comentários sobre o Agendamento</label>
-                    <input type="text" id="appointment_comments" name="appointment_comments" className="form-control" value={appointment.appointment_comments} readOnly />
-                </div>
-                <div className="form-group">
-                    <label className="form-label" htmlFor="appointment_create_date">Data de Criação</label>
-                    <input type="date" id="appointment_create_date" name="appointment_create_date" className="form-control" value={appointment.create_date} readOnly />
-                </div>
+              <div className="form-group">
+                <label className="form-label" htmlFor="appointment_comments">Comentários sobre o Agendamento</label>
+                <input type="text" id="appointment_comments" name="appointment_comments" className="form-control" value={appointment.appointment_comments} readOnly />
+              </div>
+              <div className="form-group">
+                <label className="form-label" htmlFor="appointment_create_date">Data de Criação</label>
+                <input type="text" id="appointment_create_date" name="appointment_create_date" className="form-control" value={appointment.appointment_create_date} readOnly />
+              </div>
               <div className="form-group p-2">
                 <button className="btn btn-outline-danger" type="button" onClick={handleDeleteAppointment}>Deletar</button>
                 <Link className="btn btn-outline-info" href="/admin/appointments">Voltar</Link>
